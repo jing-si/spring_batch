2579a35212a5241d71406e2e3df9512f30446e05
Dr.J (정의원) <82809765+jing-si@users.noreply.github.com>
2023-02-05 01:50:51 +0900

    no message

--- spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
+++ spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
@@ -1,16 +1,18 @@
 package spring.batch.run.job;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring.batch.run.incrementer.CustomJobParametersIncementer;
+import spring.batch.run.vlidator.CustomJobParametersValidator;
 
 @RequiredArgsConstructor
 @Configuration
@@ -24,6 +26,14 @@ public class JobConfiguration {
                 .start(step1())
                 .next(step2())
                 .next(step3())
+                //.validator(new CustomJobParametersValidator())
+                //.validator(new DefaultJobParametersValidator(new String[]{"name","date"},new String[]{"count"}))
+                //.incrementer(new CustomJobParametersIncementer())
+                .incrementer(new RunIdIncrementer())
+
+                //Job이 수행되기전에 JobRepository의 기능이 시작되기 전 단계
+                //Job이 수행되기 직전에
+                //총 2번의 검증단계를 거침
                 .build();
     }
     @Bean
@@ -48,6 +58,8 @@ public class JobConfiguration {
                         System.out.println("=======================");
                         System.out.println(">>step1  Batch!!");
                         System.out.println("=======================");
+                       // chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
+                        //stepContribution.setExitStatus(ExitStatus.STOPPED);
                         return RepeatStatus.FINISHED;
                     }
                 }).build();
@@ -62,6 +74,7 @@ public class JobConfiguration {
                         System.out.println("=======================");
                         System.out.println(">>step3  Batch!!");
                         System.out.println("=======================");
+
                         return RepeatStatus.FINISHED;
                     }
                 }).build();

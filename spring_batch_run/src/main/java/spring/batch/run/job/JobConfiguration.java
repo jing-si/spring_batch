3cd607d33701137a28e5ef4c1048a5c89c37e199
Dr.J (정의원) <82809765+jing-si@users.noreply.github.com>
2023-02-05 13:06:47 +0900

    no message

--- spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
+++ spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
@@ -6,11 +6,17 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.*;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -24,24 +30,14 @@ public class JobConfiguration {
                 .start(step1())
                 .next(step2())
                 .next(step3())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet((stepContribution, chunkContext) -> {
 
-
-                    System.out.println("=======================");
-                    System.out.println(">>step2  Batch!!");
-                    System.out.println("=======================");
-                    return RepeatStatus.FINISHED;
-                }).build();
-    }
 
     @Bean
     public Step step1(){
-        return stepBuilderFactory.get("step1")
+        return stepBuilderFactory.get("Simple step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
@@ -53,19 +49,70 @@ public class JobConfiguration {
                 }).build();
 
     }
+
     @Bean
-    public Step step3(){
-        return stepBuilderFactory.get("step3")
-                .tasklet(new Tasklet() {
+    public Step step2() {
+        return stepBuilderFactory.get("Chunk step2")
+                .<String,String> chunk(3)
+                .reader(new ItemReader<String>() {
                     @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("=======================");
-                        System.out.println(">>step3  Batch!!");
-                        System.out.println("=======================");
-                        return RepeatStatus.FINISHED;
+                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                        return null;
+                    }
+                })
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        return null;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> items) throws Exception {
+
                     }
                 }).build();
+    }
 
+    @Bean
+    public Step step3(){
+        return stepBuilderFactory.get("Partitioner step3")
+                .partitioner(step1())
+                .gridSize(2)
+                .build();
+
+    }
+    @Bean
+    public Step step4(){
+        return stepBuilderFactory.get("Job step4")
+                .job(job())
+                .build();
+
+    }
+    @Bean
+    public Step step5(){
+        return stepBuilderFactory.get("flow step5")
+                .flow(flow())
+                .build();
+
+
+    }
+
+
+    @Bean
+    public Flow flow(){
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+        flowBuilder.start(step2()).end();
+        return flowBuilder.build();
+    }
+
+    @Bean
+    public Job job(){
+        return jobBuilderFactory.get("BatchJob")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .build();
     }
 
 

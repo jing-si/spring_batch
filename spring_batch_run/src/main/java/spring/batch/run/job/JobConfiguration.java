bc22bd29db05dabb4df48119bfe83ced3a366ece
Dr.J (정의원) <82809765+jing-si@users.noreply.github.com>
2023-02-05 21:51:24 +0900

    no message

--- spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
+++ spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -21,6 +22,7 @@ public class JobConfiguration {
     @Bean
     public Job BatchJob(){
         return jobBuilderFactory.get("BatchJob")
+                .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .next(step2())
                 .next(step3())

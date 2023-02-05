1d127df27f42a7ae738e6f8a12b17115c9be0e45
Dr.J (정의원) <82809765+jing-si@users.noreply.github.com>
2023-02-05 23:56:16 +0900

    Transition의 on 메서드로 분기를 만들어 본 예제

--- spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
+++ spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
@@ -1,6 +1,7 @@
 package spring.batch.run.job;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -24,8 +25,19 @@ public class JobConfiguration {
         return jobBuilderFactory.get("BatchJob")
                 .incrementer(new RunIdIncrementer())
                 .start(step1())
-                .next(step2())
-                .next(step3())
+                    .on("FAILED")
+                    .to(step2())
+                    .on("FAILED")
+                    .stop()
+                .from(step1())
+                    .on("*")
+                    //구체적인 것이 먼저 적용된다.
+                    .to(step3())
+                    .next(step4())
+                .from(step2())
+                    .on("*")
+                    .to(step5())
+                    .end()
                 .build();
     }
     @Bean
@@ -37,6 +49,10 @@ public class JobConfiguration {
                     System.out.println("=======================");
                     System.out.println(">>step2  Batch!!");
                     System.out.println("=======================");
+                    stepContribution.setExitStatus(ExitStatus.FAILED);
+                    //steExitStatus 가 FAILE면
+                    //transition이 STOP()을 실행해서
+                    //JOB Status와 Exit_Code 는 STOP이된다.
                     return RepeatStatus.FINISHED;
                 }).build();
     }
@@ -47,9 +63,11 @@ public class JobConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+
                         System.out.println("=======================");
                         System.out.println(">>step1  Batch!!");
                         System.out.println("=======================");
+                        stepContribution.setExitStatus(ExitStatus.FAILED);
                         return RepeatStatus.FINISHED;
                     }
                 }).build();
@@ -63,6 +81,34 @@ public class JobConfiguration {
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("=======================");
                         System.out.println(">>step3  Batch!!");
+                        System.out.println("=======================");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+
+    }
+    @Bean
+    public Step step4(){
+        return stepBuilderFactory.get("step4")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("=======================");
+                        System.out.println(">>step4  Batch!!");
+                        System.out.println("=======================");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+
+    }
+    @Bean
+    public Step step5(){
+        return stepBuilderFactory.get("step5")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("=======================");
+                        System.out.println(">>step5  Batch!!");
                         System.out.println("=======================");
                         return RepeatStatus.FINISHED;
                     }

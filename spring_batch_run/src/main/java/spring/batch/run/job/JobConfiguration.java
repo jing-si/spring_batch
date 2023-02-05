87be7a720870a301de69ccd3563ef1a86de94643
Dr.J (정의원) <82809765+jing-si@users.noreply.github.com>
2023-02-05 21:31:37 +0900

    no message

--- spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
+++ spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -21,11 +23,30 @@ public class JobConfiguration {
     @Bean
     public Job BatchJob(){
         return jobBuilderFactory.get("BatchJob")
-                .start(step1())
-                .next(step2())
+                .start(flowA())
                 .next(step3())
+                .next(flowB())
+                .next(step6())
+                .end()
                 .build();
     }
+
+    private Flow flowA() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowA");
+        flowBuilder.start(step1())
+                .next(step2())
+                .end();
+        return flowBuilder.build();
+    }
+
+    private Flow flowB() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowB");
+        flowBuilder.start(step4())
+                .next(step5())
+                .end();
+        return flowBuilder.build();
+    }
+
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
@@ -68,6 +89,48 @@ public class JobConfiguration {
 
     }
 
+    @Bean
+    public Step step4(){
+        return stepBuilderFactory.get("step4")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("=======================");
+                        System.out.println(">>step4  Batch!!");
+                        System.out.println("=======================");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+
+    }
+    @Bean
+    public Step step5(){
+        return stepBuilderFactory.get("step5")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("=======================");
+                        System.out.println(">>step5  Batch!!");
+                        System.out.println("=======================");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+
+    }
+    @Bean
+    public Step step6(){
+        return stepBuilderFactory.get("step6")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("=======================");
+                        System.out.println(">>step6  Batch!!");
+                        System.out.println("=======================");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+
+    }
 
 
 }

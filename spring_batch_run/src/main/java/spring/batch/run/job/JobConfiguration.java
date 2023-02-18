2b8ac9c2eaba99369a7d9b1ef5fe21c887845044
Dr.J (정의원) <82809765+jing-si@users.noreply.github.com>
2023-02-18 15:18:54 +0900

    부모 JOB에서 자식 JOB 실행 - 실패

--- spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
+++ spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
@@ -2,22 +2,38 @@ package spring.batch.run.job;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 
 @RequiredArgsConstructor
 @Configuration
 public class JobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private TaskExecutor taskExecutor;
+
+    @Autowired
+    private BasicBatchConfigurer basicBatchConfigurer;
 
     @Bean
     public Job BatchJob(){
@@ -32,11 +48,18 @@ public class JobConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((stepContribution, chunkContext) -> {
+                    Job childJob = jobBuilderFactory.get("childJob")
+                            .incrementer(new RunIdIncrementer())
+                            .start(step1())
+                            .next(step3())
+                            .build();
+                    SimpleJobLauncher jobLauncher1 = (SimpleJobLauncher)basicBatchConfigurer.getJobLauncher();
+                    jobLauncher1.setTaskExecutor(new SimpleAsyncTaskExecutor());
 
+                    JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+                    jobParametersBuilder.addString("Id", "id1");
+                    jobLauncher.run(childJob, jobParametersBuilder.toJobParameters());
 
-                    System.out.println("=======================");
-                    System.out.println(">>step2  Batch!!");
-                    System.out.println("=======================");
                     return RepeatStatus.FINISHED;
                 }).build();
     }

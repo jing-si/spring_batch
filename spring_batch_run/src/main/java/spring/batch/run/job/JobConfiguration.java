064e6150ff099043f252f781d6a5addde252104d
Dr.J (정의원) <82809765+jing-si@users.noreply.github.com>
2023-02-19 17:22:22 +0900

    Chunk의 기본적인 사용 방식

--- spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
+++ spring_batch_run/src/main/java/spring/batch/run/job/JobConfiguration.java
@@ -9,9 +9,16 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -29,6 +36,31 @@ public class JobConfiguration {
                 .build();
     }
     @Bean
+    public Step step1(){
+        return stepBuilderFactory.get("step1")
+                //<>는 input, output 타입을 정하기 위한 것
+                .<String,String>chunk(5)
+                .reader(new ListItemReader<>(Arrays.asList("item1","item2","item3","item4","item5","item6")))
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        Thread.sleep(300);
+                        System.out.println("item = "+item );
+                        return "my" + item;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> items) throws Exception {
+                        Thread.sleep(300);
+                        System.out.println("items = "+items);
+                    }
+                })
+                .build();
+
+    }
+
+    @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((stepContribution, chunkContext) -> {
@@ -41,20 +73,7 @@ public class JobConfiguration {
                 }).build();
     }
 
-    @Bean
-    public Step step1(){
-        return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("=======================");
-                        System.out.println(">>step1  Batch!!");
-                        System.out.println("=======================");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
 
-    }
     @Bean
     public Step step3(){
         return stepBuilderFactory.get("step3")
